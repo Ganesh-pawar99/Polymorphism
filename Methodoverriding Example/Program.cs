ad68e22450648be4542d9c665ea9419e8e9f1d5c
Ganesh-pawar99 <149935100+Ganesh-pawar99@users.noreply.github.com>
2024-01-04 17:49:23 +0530

    Add files via upload

---
+++ Methodoverriding Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverriding_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.print(); 
+
+            Console.ReadLine();
+        }
+    }
+    public class A
+    {
+        public void print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    public class B :A
+    {
+        public new void print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+}
